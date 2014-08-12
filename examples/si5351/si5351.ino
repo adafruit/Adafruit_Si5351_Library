e9009d8f313dd5d3e23df6e876496dba1c139fc5
ladyada <limor@ladyada.net>
2014-08-12 15:57:13 -0400

    comments

--- examples/si5351/si5351.ino
+++ examples/si5351/si5351.ino
@@ -25,13 +25,14 @@ void setup(void)
   /* INTEGER ONLY MODE --> most accurate output */  
   /* Setup PLLA to integer only mode @ 900MHz (must be 600..900MHz) */
   /* Set Multisynth 0 to 112.5MHz using integer only mode (div by 4/6/8) */
+  /* 25MHz * 36 = 900 MHz, then 900 MHz / 8 = 112.5 MHz */
   Serial.println("Set PLLA to 900MHz");
   clockgen.setupPLLInt(SI5351_PLL_A, 36);
   Serial.println("Set Output #0 to 112.5MHz");  
   clockgen.setupMultisynthInt(0, SI5351_PLL_A, SI5351_MULTISYNTH_DIV_8);
 
   /* FRACTIONAL MODE --> More flexible but introduce clock jitter */
-  /* Setup PLLB to fractional mode @616.66667MHz (XTAL*24.66667) */
+  /* Setup PLLB to fractional mode @616.66667MHz (XTAL * 24 + 2/3) */
   /* Setup Multisynth 1 to 13.55311MHz (PLLB/45.5) */
   clockgen.setupPLL(SI5351_PLL_B, 24, 2, 3);
   Serial.println("Set Output #1 to 13.553115MHz");  
@@ -42,7 +43,7 @@ void setup(void)
   /* then divide by 64 for 10.706 KHz */
   /* configured using either PLL in either integer or fractional mode */
 
-  Serial.println("Set Output #2 to 10.7 KHz");  
+  Serial.println("Set Output #2 to 10.706 KHz");  
   clockgen.setupMultisynth(2, SI5351_PLL_B, 900, 0, 1);
   clockgen.setupRdiv(2, SI5351_R_DIV_64);
     
