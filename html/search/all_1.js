a79e68255d5df8cb4dadba50eed93485589e8d37
Doxygen CI <ci-arduino@invalid>
2021-10-12 18:58:28 +0000

    Deploy docs to GitHub Pages from commit 21adb3a228
    Commit: 21adb3a22856ded3f23a9e8d88d3b0781c4cda88
    GitHub Actions run: 1334455633

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___s_i5351.html#aa82576d548ad847bdb27757008f7b046',1,'Adafruit_SI5351']]]
+  ['begin',['begin',['../class_adafruit___s_i5351.html#a18ca48ba2e05fbf77250d7b108798d76',1,'Adafruit_SI5351']]]
 ];
--- html/search/enumvalues_0.js
+++ html/search/enumvalues_0.js
@@ -6,6 +6,7 @@ var searchData=
   ['error_5fi2c_5fdevicenotfound',['ERROR_I2C_DEVICENOTFOUND',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698aa0f0cd630de098afe2510419f633f331',1,'errors.h']]],
   ['error_5fi2c_5fnoack',['ERROR_I2C_NOACK',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698aabbe6d9cea0f0f65b1625783ff16ec02',1,'errors.h']]],
   ['error_5fi2c_5ftimeout',['ERROR_I2C_TIMEOUT',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698ac11d149980eadd9b75f78306e5a42b62',1,'errors.h']]],
+  ['error_5fi2c_5ftransaction',['ERROR_I2C_TRANSACTION',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698a0d29c7e695f5276227264304a88294f3',1,'errors.h']]],
   ['error_5finvalidparameter',['ERROR_INVALIDPARAMETER',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698a5865b128f56c4446f421ca145df924e4',1,'errors.h']]],
   ['error_5fnone',['ERROR_NONE',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698a4edc8aade11e1de7bbbdc04704baae5f',1,'errors.h']]],
   ['error_5foperationtimedout',['ERROR_OPERATIONTIMEDOUT',['../errors_8h.html#a5edd7aad54ac059bf561cf2f92017698a486b5c1ff1c4c4b7b48fcb9aac582069',1,'errors.h']]],
