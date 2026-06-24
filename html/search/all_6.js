b53a478141bbc4eee07426579e0fef4e535b6606
Doxygen CI <ci-arduino@invalid>
2026-06-24 22:24:21 +0000

    Deploy docs to GitHub Pages from commit d57a37bc56
    Commit: d57a37bc56cf7e40db3e7324c7761623f6c8975a
    GitHub Actions run: 28133297068

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setclockbuilderdata',['setClockBuilderData',['../class_adafruit___s_i5351.html#a792d32ecb4db688642ce8b5b8ea793d5',1,'Adafruit_SI5351']]],
+  ['setfrequency',['setFrequency',['../class_adafruit___s_i5351.html#a43eaf5845fa56034b4958a0339230a6c',1,'Adafruit_SI5351']]],
   ['setupmultisynth',['setupMultisynth',['../class_adafruit___s_i5351.html#a7a38f8de810f72408156ba68ee3c04c4',1,'Adafruit_SI5351']]],
   ['setupmultisynthint',['setupMultisynthInt',['../class_adafruit___s_i5351.html#a5c92087c9ade21fad1ac7b85cf67e4fb',1,'Adafruit_SI5351']]],
   ['setuppll',['setupPLL',['../class_adafruit___s_i5351.html#a41a4de34f331294d79fdcc8cc7090916',1,'Adafruit_SI5351']]],
