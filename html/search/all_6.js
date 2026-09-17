fa5ff52947d6c97e22387f18a2bbfe89e1ba330e
Doxygen CI <ci-arduino@invalid>
2026-09-17 19:55:53 +0000

    Deploy docs to GitHub Pages from commit 03cb760031
    Commit: 03cb7600316c359c5c14dc4d46a50a2c7e90af94
    GitHub Actions run: 35265781570

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['readdevicestatus',['readDeviceStatus',['../class_adafruit___s_i5351.html#adef781614acaf338f9e5a81351211423',1,'Adafruit_SI5351']]],
-  ['readstickystatus',['readStickyStatus',['../class_adafruit___s_i5351.html#a73c08c14bcddf93a7c1366c218ee85c8',1,'Adafruit_SI5351']]]
+  ['readstickystatus',['readStickyStatus',['../class_adafruit___s_i5351.html#a73c08c14bcddf93a7c1366c218ee85c8',1,'Adafruit_SI5351']]],
+  ['resetpll',['resetPLL',['../class_adafruit___s_i5351.html#a18c9551f8cd31976e08a873d3052de96',1,'Adafruit_SI5351']]]
 ];
